a6ff32d8f3987bcafc0c9262e494bb685059c5b9
SreekaraPenugonda <sreekara2006@gmail.com>
2025-11-03 16:04:48 +0530

    Refactor main.jsx to use React.StrictMode
    Removed routing and added StrictMode for rendering.

--- Todo/src/main.jsx
+++ Todo/src/main.jsx
@@ -1,14 +1,10 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import App from './App';
-import Login from './components/Login';
-import Signup from './components/Signup';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/login" element={<Login onAuth={...} />} />
-      <Route path="/signup" element={<Signup onAuth={...} />} />
-    </Routes>
-  </BrowserRouter>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
