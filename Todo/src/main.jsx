7267500eace0710d7d16310df67f3e161fa4c76f
SreekaraPenugonda <sreekara2006@gmail.com>
2025-11-03 15:58:23 +0530

    Update main.jsx

--- Todo/src/main.jsx
+++ Todo/src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './styles/app.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import App from './App';
+import Login from './components/Login';
+import Signup from './components/Signup';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/login" element={<Login onAuth={...} />} />
+      <Route path="/signup" element={<Signup onAuth={...} />} />
+    </Routes>
+  </BrowserRouter>
+);
